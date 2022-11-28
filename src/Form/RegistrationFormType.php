2c1f2c158cec59bd9f5bb3cdaa4186f29a4011a9
VentoAureo230 <anthony.meny35@gmail.com>
2022-11-28 17:16:58 +0100

    Login fonctionnel sur les routes attribués

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,14 +18,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            ->add('firstname')
+            ->add('lastname')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
