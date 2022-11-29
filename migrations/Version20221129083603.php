9ffd562d98d1065504e371b4c4011d00ebf5f895
VentoAureo230 <anthony.meny35@gmail.com>
2022-11-29 09:47:50 +0100

    Màj BDD + Login / Register fonctionnel

---
+++ migrations/Version20221129083603.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221129083603 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD email VARCHAR(180) NOT NULL, ADD roles JSON NOT NULL, ADD lastname VARCHAR(255) NOT NULL, DROP name, DROP mail, DROP is_admin, CHANGE experience experience INT DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user ADD mail VARCHAR(255) NOT NULL, ADD is_admin TINYINT(1) NOT NULL, DROP email, DROP roles, CHANGE experience experience INT NOT NULL, CHANGE lastname name VARCHAR(255) NOT NULL');
+    }
+}
