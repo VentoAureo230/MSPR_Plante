d6875b876c27f290fe71ba68a4dfa9fdc7fefe9d
Antoine <thoino@outlook.fr>
2022-12-10 20:12:55 +0100

    correction bug

--- src/Controller/Admin/PictureCrudController.php
+++ src/Controller/Admin/PictureCrudController.php
@@ -18,10 +18,9 @@ class PictureCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
-        return [
+        return [            
             ImageField::new('file_name')->setBasePath('public\uploads\images\plant')->setUploadDir('public\uploads\images\plant')->setFormTypeOption('allow_delete', false),
             TextField::new('file',':')->onlyWhenUpdating()->setFormType(VichImageType::class)->setFormTypeOption('allow_delete', false)->setFormTypeOption('allow_file_upload', false)->setFormTypeOption('disabled','disabled')
         ];
-    }
-    
+    } 
 }
