1ffd2561ffaca11b376195a81dbb78caf25ff749
VentoAureo230 <anthony.meny35@gmail.com>
2022-12-12 19:13:20 +0100

    Bug fix du modif user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 use App\Form\UserType;
+use App\Form\UserPasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use App\Entity\User;
-use App\Form\UserPasswordType;
 
 class UserController extends AbstractController
 {
@@ -26,29 +27,29 @@ class UserController extends AbstractController
         return $this->render('stats/index.html.twig');
     }
 
-    /**
-     * Fonction de modification des informations de l'utilisateur
-     */
     #[Route('/user/edit_profile', name: 'app_edit_user')]
-    public function edit(Request $request, ManagerRegistry $manager, UserPasswordHasherInterface $hasher): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login'); // Si le User n'est pas co il est redirigé au login
         }
 
-        $form = $this->createForm(UserType::class, $this->getUser());
+        $form = $this->createForm(UserType::class, $user = $this->getUser());
 
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {}
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setFirstname($form->get('firstname')->getData());
+            $user->setLastname($form->get('lastname')->getData());
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
 
         return $this->render('user/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
-    /**
-     * Fonction de modification du mot de passe de l'utilisateur
-     */
     #[Route('/user/edit_password', name: 'app_edit_password', methods: ['GET', 'POST'])]
     public function editPassword(User $user, Request $request, ManagerRegistry $manager, UserPasswordHasherInterface $hasher): Response
     {
